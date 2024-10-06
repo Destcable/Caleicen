afeda1133e4ff7a3751f927f18eeeea2cc5d277a
Destcable <artmobpavlov21@gmail.com>
2024-10-06 18:06:19 +0300

    upd layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,11 +3,10 @@ import { sidebarItemsOptions } from "../../core/config/sidebarItemsOptions.confi
 import { Sidebar } from "../Sidebar/Sidebar";
 import { SidebarItem } from "../Sidebar/SidebarItem";
 import { SidebarItems } from "../Sidebar/SidebarItems";
-import { Navigate, Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { APP_ROUTE_LOGIN } from "../../core/config/app.config";
 
 const Layout = () => {
-    const navigate = useNavigate();
     const [selectedSidebarItem, setSelectedSidebarItem] = useState<string>("Calendar");
 
     const handleClickSidebarItem = (value: string) => setSelectedSidebarItem(value);
